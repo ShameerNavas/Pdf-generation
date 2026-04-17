8484c8291a12597716d3213b912b2fa53048e50a
Shameer Navas <shameernavasofficial@gmail.com>
2026-04-17 22:25:02 +0300

    PdfService.cs

---
+++ PdfService.cs
@@ -0,0 +1,27 @@
+﻿using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using System.IO;
+
+namespace PDF_generation.Services
+{
+    public class PdfService
+    {
+        public byte[] GeneratePdf(string content)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                using (var writer = new PdfWriter(stream))
+                {
+                    using (var pdf = new PdfDocument(writer))
+                    {
+                        var document = new Document(pdf);
+                        document.Add(new Paragraph(content));
+                    }
+                }
+
+                return stream.ToArray();
+            }
+        }
+    }
+}
